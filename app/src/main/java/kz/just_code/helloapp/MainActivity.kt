caee5dd961d3737cafc646428b58bcbd841de379
Vampirish <vamperish@gmail.com>
2023-11-15 23:48:50 +0600

    Last commit

--- app/src/main/java/kz/just_code/helloapp/MainActivity.kt
+++ app/src/main/java/kz/just_code/helloapp/MainActivity.kt
@@ -10,37 +10,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import kz.just_code.helloapp.ui.theme.HelloAppTheme
+import kz.just_code.helloapp.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            HelloAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpName("Jack")
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HelloAppTheme {
-        Greeting("Android")
+    private fun setUpName(name: String) {
+        binding.nameView.text = getString(R.string.hello_my_name, name)
     }
 }
